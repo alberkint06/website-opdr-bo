7ef020528fff58c58d63a940efbe99d32d5dfa69
Luke <35667@ma-web.nl>
2023-01-18 12:49:25 +0100

    created holes for whack-a-mole in js

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ class App
 {
     runApplication()
     {
-        console.log("hello world");
-        //code gaat hier!
     }
 }
 
@@ -15,6 +13,83 @@ console.log(canvas);
 
 let g = canvas.getContext("2d");
 
+
+//bg brown
 g.beginPath();
-g.rect(500, 20, 700, 700);
+g.fillStyle = "#964B00";
+g.rect(500, 20, 720, 720);
+g.closePath();
 g.stroke();
+g.fill();
+
+//hole0 top left
+g.beginPath();
+g.fillStyle = "#483C32";
+g.rect(530, 50, 200, 200);
+g.closePath();
+g.stroke();
+g.fill();
+
+//hole1 top middle
+g.beginPath();
+g.fillStyle = "#483C32";
+g.rect(760, 50, 200, 200);
+g.closePath();
+g.stroke();
+g.fill();
+
+//hole2 top right
+g.beginPath();
+g.fillStyle = "#483C32";
+g.rect(990, 50, 200, 200);
+g.closePath();
+g.stroke();
+g.fill();
+
+//hole3 middle left
+g.beginPath();
+g.fillStyle = "#483C32";
+g.rect(530, 280, 200, 200);
+g.closePath();
+g.stroke();
+g.fill();
+
+//hole4 middle
+g.beginPath();
+g.fillStyle = "#483C32";
+g.rect(760, 280, 200, 200);
+g.closePath();
+g.stroke();
+g.fill();
+
+//hole5 middle right
+g.beginPath();
+g.fillStyle = "#483C32";
+g.rect(990, 280, 200, 200);
+g.closePath();
+g.stroke();
+g.fill();
+
+//hole6 bottom left
+g.beginPath();
+g.fillStyle = "#483C32";
+g.rect(530, 510, 200, 200);
+g.closePath();
+g.stroke();
+g.fill();
+
+//hole7 bottom middle
+g.beginPath();
+g.fillStyle = "#483C32";
+g.rect(760, 510, 200, 200);
+g.closePath();
+g.stroke();
+g.fill();
+
+//hole8 bottom right
+g.beginPath();
+g.fillStyle = "#483C32";
+g.rect(990, 510, 200, 200);
+g.closePath();
+g.stroke();
+g.fill();
